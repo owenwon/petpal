9dfed5c06afacf9c09d30a2dab9ec84d2407ee6e
Owen Wong <owenwong@uw.edu>
2026-03-08 12:56:57 -0700

    app initial commit

---
+++ petpal/petpal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  petpal
+//
+//  Created by Owen Wong on 3/8/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ petpal/petpalTests/petpalTests.swift
@@ -0,0 +1,17 @@
+//
+//  petpalTests.swift
+//  petpalTests
+//
+//  Created by Owen Wong on 3/8/26.
+//
+
+import Testing
+@testable import petpal
+
+struct petpalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
